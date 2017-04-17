ee5b6f00b4b2156d0eb11530497884c7bb31fa98
Bethany Jackson <bnjacks487@gmail.com>
2017-04-17 06:38:03 +0000

    Final commit

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,31 +4,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "BJ Records"
+    assert_select "title", "Vintage Vinyls"
   end
 
   test "should get shop" do
     get shop_path
     assert_response :success
-    assert_select "title", "Shop | BJ Records"
+    assert_select "title", "Shop | Vintage Vinyls"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | BJ Records"
+    assert_select "title", "About | Vintage Vinyls"
   end
 
   test "should get reviews" do
     get reviews_path
     assert_response :success
-    assert_select "title", "Reviews | BJ Records"
+    assert_select "title", "Reviews | Vintage Vinyls"
   end
   
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | BJ Records"
+    assert_select "title", "Contact | Vintage Vinyls"
   end
 
 end
